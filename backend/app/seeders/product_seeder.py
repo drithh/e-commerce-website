0cd5d14a7673b9c6d3cef92d8607753323fabd81
drithh <adrielalfeus@student.uns.ac.id>
2022-10-16 02:08:08 +0700

    remove unused variable

--- backend/app/seeders/product_seeder.py
+++ backend/app/seeders/product_seeder.py
@@ -4,7 +4,7 @@ from app.models.product import Product
 def product_seed(fake, session, category_id):
     product_id = []
 
-    for i in range(60):
+    for _ in range(60):
         random_category_id = category_id[
             fake.random_int(min=0, max=len(category_id) - 1, step=1)
         ]
