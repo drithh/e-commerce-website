c71f41ec342c77bd95da6b190b3161d41dc27390
drithh <adrielalfeus@student.uns.ac.id>
2022-10-26 15:55:30 +0700

    add cloud_storage link

--- backend/app/api/images.py
+++ backend/app/api/images.py
@@ -1,9 +1,10 @@
 from typing import Any, Generator
 
-from fastapi import status
+from fastapi import HTTPException, status
 from fastapi.params import Depends
 from fastapi.routing import APIRouter
 
+from app.core.config import settings
 from app.core.logger import logger
 from app.deps.db import get_db
 from app.models.image import Image
@@ -17,4 +18,14 @@ async def get_image(
     image_name: str,
     session: Generator = Depends(get_db),
 ) -> Any:
-    return session.query(Image).filter(Image.name.like(f"%{image_name}%")).first()
+    image_name = image_name.lower()
+    image = session.query(Image).filter(Image.name.like(f"%{image_name}%")).first()
+    if not image:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Image not found",
+        )
+    return GetImage(
+        name=image.name,
+        image_url=f"{settings.CLOUD_STORAGE}/{image.image_url}",
+    )
--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -42,6 +42,8 @@ class Settings(BaseSettings):
 
     SECRET_KEY: str
 
+    CLOUD_STORAGE: str
+
     # Email
     MAIL_USERNAME: str
     MAIL_PASSWORD: str
