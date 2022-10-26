9d370e694ca3e8329d9f0e96b5e20360776781ef
drithh <adrielalfeus@student.uns.ac.id>
2022-10-26 13:49:06 +0700

    categories

--- frontend/src/components/OverlayContainer.tsx
+++ frontend/src/components/OverlayContainer.tsx
@@ -18,13 +18,9 @@ const OverlayContainer: FC<Props> = ({ imgSrc, imgAlt, children }) => (
     />
 
     {children}
-    <div className="top-0 h-full w-full bg-gray-500 opacity-50 group-hover:absolute"></div>
-    <div className="absolute left-[10%] bottom-[10%] h-0 w-0 transition-[width,_height] delay-[0s,_0.3s] duration-300 group-hover:h-[80%] group-hover:w-[80%] group-hover:border-b group-hover:border-l group-hover:border-l-white group-hover:border-b-white "></div>
-    <div className="absolute left-[10%] top-[10%] h-0 w-0 transition-[width,_height] delay-[0.6s,_0.9s] duration-300 group-hover:h-[80%] group-hover:w-[80%] group-hover:border-t group-hover:border-r group-hover:border-r-white group-hover:border-t-white "></div>
-
-    {/* <div className={styles.imgOverlay}></div>
-    <div className={styles.overlayBorder}></div>
-    <div className={styles.overlayBorder2}></div> */}
+    <div className="top-0 h-full w-full bg-gray-700 opacity-50 group-hover:absolute"></div>
+    <div className="absolute right-[10%] bottom-[10%] h-0 w-0 group-hover:transition-[width,_height] group-hover:delay-[0s,_0.3s] group-hover:duration-300 group-hover:h-[80%] group-hover:w-[80%] group-hover:border-b group-hover:border-l  group-hover:border-b-white group-hover:border-l-white"></div>
+    <div className="absolute left-[10%] top-[10%] h-0 w-0 group-hover:transition-[width,_height] group-hover:delay-[0.6s,_0.9s] group-hover:duration-300 group-hover:h-[80%] group-hover:w-[80%] group-hover:border-t group-hover:border-r group-hover:border-r-white group-hover:border-t-white "></div>
   </div>
 );
 
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,55 +5,97 @@ import LinkButton from '../components/button/LinkButton';
 import OverlayContainer from '../components/OverlayContainer';
 
 const Home = () => {
+  const data = [
+    {
+      imgSrc: '/img/banner_minipage2.webp',
+      imgAlt: 'Hat Collection',
+      title: 'Hat Collection',
+    },
+    {
+      imgSrc: '/img/banner_minipage3.webp',
+      imgAlt: 'Shoes Collection',
+      title: 'Shoes Collection',
+    },
+    {
+      imgSrc: '/img/banner_minipage2.webp',
+      imgAlt: 'Hat Collection',
+      title: 'Hat Collection',
+    },
+    {
+      imgSrc: '/img/banner_minipage3.webp',
+      imgAlt: 'Shoes Collection',
+      title: 'Shoes Collection',
+    },
+    {
+      imgSrc: '/img/banner_minipage2.webp',
+      imgAlt: 'Hat Collection',
+      title: 'Hat Collection',
+    },
+    {
+      imgSrc: '/img/banner_minipage3.webp',
+      imgAlt: 'Shoes Collection',
+      title: 'Shoes Collection',
+    },
+    {
+      imgSrc: '/img/banner_minipage2.webp',
+      imgAlt: 'Hat Collection',
+      title: 'Hat Collection',
+    },
+    {
+      imgSrc: '/img/banner_minipage3.webp',
+      imgAlt: 'Shoes Collection',
+      title: 'Shoes Collection',
+    },
+    {
+      imgSrc: '/img/banner_minipage2.webp',
+      imgAlt: 'Hat Collection',
+      title: 'Hat Collection',
+    },
+    {
+      imgSrc: '/img/banner_minipage3.webp',
+      imgAlt: 'Shoes Collection',
+      title: 'Shoes Collection',
+    },
+    {
+      imgSrc: '/img/banner_minipage2.webp',
+      imgAlt: 'Hat Collection',
+      title: 'Hat Collection',
+    },
+    {
+      imgSrc: '/img/banner_minipage3.webp',
+      imgAlt: 'Shoes Collection',
+      title: 'Shoes Collection',
+    },
+  ];
+
   return (
     <>
       <main id="main-content" className="mt-20 min-h-[60vh]">
         <Banner />
 
         {/* ===== Category Section ===== */}
-        <section className="border-gray100 h-auto w-full border border-b-2 py-10">
-          <div className="app-max-width app-x-padding grid h-full grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
-            <div className="w-full sm:col-span-2 lg:col-span-2">
-              <OverlayContainer
-                imgSrc="/img/banner_minipage1.webp"
-                imgAlt="New Arrivals"
-              >
-                <LinkButton
-                  href="/product-category/new-arrivals"
-                  extraClass="absolute bottom-10-per sm:right-10-per z-20"
-                >
-                  New Arrivals
-                </LinkButton>
-              </OverlayContainer>
-            </div>
-            <div className="w-full">
-              <OverlayContainer
-                imgSrc="/img/banner_minipage2.webp"
-                imgAlt="Women Collection"
-              >
-                <LinkButton
-                  href="/product-category/women"
-                  extraClass="absolute bottom-10-per z-20"
-                >
-                  Women Collection
-                </LinkButton>
-              </OverlayContainer>
-            </div>
-            <div className="w-full">
-              <OverlayContainer
-                imgSrc="/img/banner_minipage3.webp"
-                imgAlt="Men Collection"
-              >
-                <LinkButton
-                  href="/product-category/men"
-                  extraClass="absolute bottom-10-per z-20"
-                >
-                  Men Collection
-                </LinkButton>
-              </OverlayContainer>
+        <section className="border-y-gray-100 h-auto w-full  border-b-2 py-10  ">
+          <div className="text-center my-6">
+            <h2 className="text-3xl">Our Collections</h2>
+          </div>
+          <div className="wrapper max-w-7xl mx-auto">
+            <div className="app-max-width app-x-padding grid h-full grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
+              {data.map((item, index) => (
+                <div className="w-full relative">
+                  <OverlayContainer imgSrc={item.imgSrc} imgAlt={item.imgAlt}>
+                    <LinkButton
+                      href="/product-category/women"
+                      extraClass="absolute bottom-[10%] z-20"
+                    >
+                      {item.title}
+                    </LinkButton>
+                  </OverlayContainer>
+                </div>
+              ))}
             </div>
           </div>
         </section>
+        <section className="py-4">test</section>
       </main>
     </>
   );
