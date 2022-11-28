1b50708adeafde890f42c4103f43cab70dec026e
argaputra12 <argaputra@student.uns.ac.id>
2022-11-28 23:41:16 +0700

    fix bug

--- backend/tests/api/test_products.py
+++ backend/tests/api/test_products.py
@@ -1,3 +1,6 @@
+import uuid
+
+from sqlalchemy.orm.session import Session
 from starlette.testclient import TestClient
 
 from app.core.config import settings
@@ -20,6 +23,96 @@ def test_get_products(client: TestClient, create_product):
     assert resp.status_code == 200
     data = resp.json().get("data")
     assert data[0]["id"] == str(product.id)
+
+
+def test_get_newest_products(client: TestClient, db: Session, create_category):
+    category = create_category()
+    db.execute(
+        """INSERT INTO products (title, brand, product_detail, price, category_id, condition, created_at) VALUES
+        ('product1', 'product1', 'product1', 10000, :category_id, 'new', '2022-11-02 16:36:56.324' )""",
+        {"""category_id""": category.id},
+    )
+    db.execute(
+        """INSERT INTO products (title, brand, product_detail, price, category_id, condition, created_at) VALUES
+        ('product2', 'product2', 'product2', 10000, :category_id, 'new', '2022-11-03 16:36:56.324' )""",
+        {"""category_id""": category.id},
+    )
+
+    db.commit()
+
+    resp = client.get(
+        f"{prefix}",
+        params={"sort_by": "Newest"},
+    )
+
+    assert resp.json()["data"][0]["title"] == "product2"
+    assert resp.status_code == 200
+
+
+def test_get_oldest_products(client: TestClient, db: Session, create_category):
+    category = create_category()
+    db.execute(
+        """
+            INSERT INTO products (title, brand, product_detail, price, category_id, condition, created_at) VALUES
+            ('product1', 'product1', 'product1', 10000, :category_id, 'new', '2022-11-02 16:36:56.324' )
+        """,
+        {"""category_id""": category.id},
+    )
+    db.execute(
+        """
+            INSERT INTO products (title, brand, product_detail, price, category_id, condition, created_at) VALUES
+            ('product2', 'product2', 'product2', 10000, :category_id, 'new', '2022-11-03 16:36:56.324' )
+        """,
+        {"""category_id""": category.id},
+    )
+    db.commit()
+
+    resp = client.get(
+        f"{prefix}",
+        params={"sort_by": "Oldest"},
+    )
+
+    assert resp.json()["data"][0]["title"] == "product1"
+    assert resp.status_code == 200
+
+
+def test_get_products_with_category_id(client: TestClient, create_product):
+    product = create_product()
+    category_id = product.category_id
+
+    resp = client.get(f"{prefix}", params={"category": [str(category_id)]})
+    assert resp.status_code == 200
+    assert resp.json()["data"][0]["id"] == str(product.id)
+
+
+def test_products_with_name_price_condition(
+    client: TestClient, db: Session, create_category
+):
+    category = create_category()
+    db.execute(
+        """
+            INSERT INTO products (title, brand, product_detail, price, category_id, condition) VALUES
+            ('product1', 'product1', 'product1', 10000, :category_id, 'new' )
+        """,
+        {"category_id": category.id},
+    )
+    db.commit()
+
+    resp = client.get(
+        f"{prefix}",
+        params={"product_name": "product1", "price": [5000, 20000], "condition": "new"},
+    )
+    assert resp.status_code == 200
+    assert resp.json()["data"][0]["title"] == "product1"
+
+
+def test_products_with_page_limit(client: TestClient, create_product):
+    create_product()
+    create_product()
+
+    resp = client.get(f"{prefix}", params={"page": 1, "page_size": 5})
+    assert resp.status_code == 200
+    assert len(resp.json()["data"]) == 2
 
 
 def test_get_empty_product(client: TestClient):
@@ -81,6 +174,29 @@ def test_create_product(client: TestClient, create_category, create_admin, creat
     )
     assert resp.json()["message"] == "Product added"
     assert resp.status_code == 201
+
+
+def test_create_product_wrong_category(
+    client: TestClient,  create_admin
+):
+    admin = create_admin()
+
+    resp = client.post(
+        f"{prefix}",
+        headers=get_jwt_header(admin),
+        json={
+            "title": "hehe",
+            "brand": "hoho",
+            "product_detail": "haha",
+            "images": [],
+            "price": 10000,
+            "condition": "new",
+            "category_id": str(uuid.uuid4()),
+            "stock": [{"size": "S", "quantity": 100}],
+        },
+    )
+    assert resp.status_code == 400
+    assert resp.json()["message"].startswith("IntegrityError")
 
 
 def test_update_product_not_admin(client: TestClient):
